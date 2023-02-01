48e6071205d7590d77f03205a1a27e5bbba61383
Deepak <deepakkumarkasyap12@gmail.com>
2023-02-02 03:17:32 +0530

    coming soon page

---
+++ components/coming_soon/ComingSoon.js
@@ -0,0 +1,20 @@
+// import './ComingSoon.css'
+
+export default function ComingSoon() {
+  return (
+    <div className="comingSoon">
+      <div className="eventLogo">
+        <img src="img/logo.png" alt="eventLogo" />
+      </div>
+      <div className="eventAbout">
+        <div className="heading">
+          <span>
+            TECH MAHOTSAV<i>&apos;</i> 23
+          </span>
+          <img src="img/uline.png" alt="" />
+        </div>
+        <div className="subHeading">Coming Soon...</div>
+      </div>
+    </div>
+  );
+}
