5175fde554130388813793d32a0e41dea1868fc8
Mohit Pandey <pandeymohit215@gmail.com>
2023-02-08 03:45:26 +0530

    Restructured files and folders

--- components/card/Card.js
+++ components/card/Card.js
@@ -1,21 +1,23 @@
 import Link from 'next/link';
 
+import Styles from './Card.module.css';
+
 function Card() {
   return (
-    <div className="eventCard">
+    <div className={Styles.eventCard}>
       <img src="img/e1.png" alt="" />
-      <div className="eventheading">Spider 3.0</div>
-      <div className="des">
+      <div className={Styles.eventheading}>Spider 3.0</div>
+      <div className={Styles.des}>
         <span>lorem ipsum doler sit amet</span>
         <span>
           By <b>ISTE</b>
         </span>
       </div>
-      <div className="prize">
+      <div className={Styles.prize}>
         <span>Prizes Worth</span>
-        <div className="prizeheading">INR 19500</div>
+        <div className={Styles.prizeheading}>INR 19500</div>
       </div>
-      <div className="cardBtn">
+      <div className={Styles.cardBtn}>
         <Link href="/EventsPage" legacyBehavior>
           <button type="button">Know More</button>
         </Link>
--- pages/Event.js
+++ pages/Event.js
@@ -1,16 +1,17 @@
-import Card from '../components/Card';
-import Navbar from '../components/Navbar';
+import Card from '@/components/card/Card';
+import Navbar from '@/components/navbar/Navbar';
+import Styles from '../components/card/EventCardSection.module.css';
 
 const event = () => {
   return (
     <>
       <Navbar />
-      <div className="event">
-        <div className="heading">
+      <div className={Styles.event}>
+        <div className={Styles.heading}>
           Competitions <img src="img/line.svg" alt="" />
         </div>
-        <div className="subheading">Day 1</div>
-        <div className="eventCardSection">
+        <div className={Styles.subheading}>Day 1</div>
+        <div className={Styles.eventCardSection}>
           <Card />
           <Card />
           <Card />
