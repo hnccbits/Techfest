1a05b6d3f386436b471072dce328c1fc948a69df
akm-akm <adityanick08@gmail.com>
2023-02-10 15:06:21 +0530

    axios fetching jwt for every request

--- api/axios.js
+++ api/axios.js
@@ -2,18 +2,19 @@ import axios from 'axios';
 
 let token;
 
-if (typeof window !== 'undefined') {
-  token = window.localStorage.getItem('JWTTOKEN');
-} else {
-  token = '';
-}
 const axiosInstance = axios.create({
-  baseURL:'https://techmahotsav.azurewebsites.net/api'
+   baseURL: 'https://techmahotsav.azurewebsites.net/api',
  // baseURL: 'http://localhost:5003/api',
 });
 
 axiosInstance.interceptors.request.use(
   (config) => {
+    if (typeof window !== 'undefined') {
+      token = window.localStorage.getItem('JWTTOKEN');
+    } else {
+      token = '';
+    }
+
     // eslint-disable-next-line no-param-reassign
     config.headers.Authorization = `Bearer ${token}`;
     return config;
